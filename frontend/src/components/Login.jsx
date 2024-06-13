fbdbfa6b9d5667018ebd7f854c281e740b1e609c
Shashwat Dixit <shashwatmain@gmail.com>
2024-06-13 13:26:55 +0530

    Added modals for video urls and choosing LLM models

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,8 +4,8 @@ export default function Login() {
     return (
         <div className='flex flex-col'>
             <Form method='post' className='flex flex-col'>
-                <input className="px-8 py-4 my-4 rounded" type="email" id="user_email" name="user_email" placeholder="Email Address" />
-                <input className="px-8 py-4 my-4 rounded" type="password" id="user_password" name="user_password" placeholder="Password"/>
+                <input className="px-8 py-4 my-4 rounded" type="email" id="user_email" name="user_email" placeholder="Email Address" required/>
+                <input className="px-8 py-4 my-4 rounded" type="password" id="user_password" name="user_password" placeholder="Password" required/>
                 <button type="submit">Sign In</button>
             </Form>
             <h1 className="mt-4 text-xl px-8 my-3">Forgot my password!</h1>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,9 +4,9 @@ export default function SignUp() {
     return (
         <div className='flex flex-col'>
             <Form method='post' className='flex flex-col'>
-                <input className="px-8 py-4 my-4 rounded" type="email" name="user_name" placeholder="Name" />
-                <input className="px-8 py-4 my-4 rounded" type="email" name="user_email" placeholder="Email Address" />
-                <input className="px-8 py-4 my-4 rounded" type="password" name="user_password" placeholder="Password" />
+                <input className="px-8 py-4 my-4 rounded" type="text" name="user_name" placeholder="Name" required/>
+                <input className="px-8 py-4 my-4 rounded" type="email" name="user_email" placeholder="Email Address" required/>
+                <input className="px-8 py-4 my-4 rounded" type="password" name="user_password" placeholder="Password" required/>
                 <button type="submit">Sign Up</button>
             </Form>
         </div>
