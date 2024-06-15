3f3ecedcc210ea0abf356eb80124d19e0e595195
Shashwat Dixit <shashwatmain@gmail.com>
2024-06-16 02:58:39 +0530

    Added backend starter code

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-const express = require('express');
+import express from 'express'
+import { PORT } from './config/config.js';
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
