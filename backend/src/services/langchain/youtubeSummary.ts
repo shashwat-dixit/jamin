365c29b41093b45773c522be7619e40235f2ddc1
Shashwat Dixit <shashwatmain@gmail.com>
2024-11-27 07:55:25 +0530

    Adding YT summary

--- backend/src/services/langchain/youtubeSummary.ts
+++ backend/src/services/langchain/youtubeSummary.ts
@@ -1,3 +1,11 @@
-import { db } from "../../db";
+import { YoutubeLoader } from "@langchain/community/document_loaders/web/youtube";
 
+const loader = YoutubeLoader.createFromUrl("https://youtu.be/bZQun8Y4L2A", {
+  language: "en",
+  addVideoInfo: true,
+});
+
+const docs = await loader.load();
+
+console.log(docs);
 
