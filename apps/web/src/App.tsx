f1d9597198d4d96a5a6070428d32d459b16cc088
Shashwat Dixit <shashwatmain@gmail.com>
2026-03-13 18:52:40 +0530

    Move to bun

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-3xl font-bold text-center'>Jamin</h1>
+    <h2 className='text-2xl font-bold text-center'>Releases on <span className="text-blue-500">March 16, 2026</span></h2>
+    </>
+  )
+}
+
+export default App
